3f419db15a068b8d75b498208365c45d0c9fe851
jungchungsub <cndtjq1248@naver.com>
2022-09-21 20:19:18 +0900

    자바 예외처리 한번에 하는번, 커스트 익셉션 만들기

--- src/main/java/site/metacoding/red/service/BoardsService.java
+++ src/main/java/site/metacoding/red/service/BoardsService.java
@@ -10,7 +10,7 @@ import site.metacoding.red.domain.boards.BoardsDao;
 import site.metacoding.red.domain.loves.Loves;
 import site.metacoding.red.domain.loves.LovesDao;
 import site.metacoding.red.domain.users.Users;
-import site.metacoding.red.domain.users.UsersDao;
+import site.metacoding.red.handler.ex.MyException;
 import site.metacoding.red.web.dto.request.boards.UpdateDto;
 import site.metacoding.red.web.dto.request.boards.WriteDto;
 import site.metacoding.red.web.dto.response.boards.DetailDto;
@@ -58,7 +58,14 @@ public class BoardsService {
 	}
 	
 	public Boards 게시글수정화면데이터가져오기(Integer id) {
-		return boardsDao.findById(id);
+
+		Boards boards = boardsDao.findById(id);
+
+		if(boards == null) {
+			throw new MyException(id+"의 게시글을 찾을 수 없습니다.");
+		}
+
+		return boards;
 	}
 
 	public void 게시글수정하기(Integer id, UpdateDto updateDto) {
@@ -66,6 +73,7 @@ public class BoardsService {
 		Boards boardsPS = boardsDao.findById(id);
 		
 		if(boardsPS == null) {
+			throw new RuntimeException(id+"의 게시글을 찾을 수 없습니다.");
 			// 이 부분은 나중에 처리!! (exception 처리하는 법 따로 배울 예정)
 		}
 
